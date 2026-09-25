1c0b269d88bed1282e3d50a150046a7d673b02a3
rfsjim <5404425+rfsjim@users.noreply.github.com>
2026-09-25 20:36:49 +1000

    Feat: Add all mod items and blocks to Mod Creative Tab

--- src/main/java/net/jimmynet/jamesindustries/item/ModCreativeModeTabs.java
+++ src/main/java/net/jimmynet/jamesindustries/item/ModCreativeModeTabs.java
@@ -1,10 +1,13 @@
 package net.jimmynet.jamesindustries.item;
 
 import net.jimmynet.jamesindustries.JamesiumIndustries;
-
+import net.jimmynet.jamesindustries.block.ModBlocks;
+import net.jimmynet.jamesindustries.helpers.EnchantHelpers;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.network.chat.Component;
 
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -25,5 +28,18 @@ public class ModCreativeModeTabs {
             .icon(() -> ModItems.MAGIC_CUBE.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
                 output.accept(ModItems.MAGIC_CUBE.get());
+
+                ItemStack enchantedNetherSword = EnchantHelpers.applyEnchantmentValidated(
+                    new ItemStack(ModItems.NETHER_SWORD.get()),
+                    parameters.holders(),
+                    Enchantments.FIRE_ASPECT);
+
+                output.accept(enchantedNetherSword);
+                output.accept(ModItems.SILVER_INGOT.get());
+                output.accept(ModItems.PET_RABBIT_SPAWN_EGG.get());
+                output.accept(ModBlocks.MAPLE_LOG_BLOCK.get());
+                output.accept(ModBlocks.RED_ORE_BLOCK.get());
+                output.accept(ModBlocks.SILVER_BLOCK.get());
+                output.accept(ModBlocks.SILVER_ORE_BLOCK.get());
             }).build());
 }
